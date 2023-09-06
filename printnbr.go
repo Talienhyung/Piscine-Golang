0580d5ca393ef6aca39f45a86edef4201a11a5ef
srivier <soleane.rivier@ynov.com>
2023-09-06 20:38:31 +0200

    message

--- printnbr.go
+++ printnbr.go
@@ -8,14 +8,20 @@ func PrintNbr(n int) {
 	}
 	if n < 0 {
 		z01.PrintRune('-')
-		n = -n
 	}
-	digits := make([]int, 3)
+
+	digits := make([]int, 0)
 	for n > 0 {
 		p := n % 10
 		digits = append(digits, p)
 		n = n / 10
 	}
+	for n < 0 {
+		p := n % 10
+		digits = append(digits, -p)
+		n = n / 10
+	}
+
 	for i := len(digits) - 1; i >= 0; i-- {
 		if digits[i] == 0 {
 			z01.PrintRune('0')
