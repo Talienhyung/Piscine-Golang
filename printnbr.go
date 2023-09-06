6aebadce638f7633ecb0c2447eb618bed99e2098
srivier <soleane.rivier@ynov.com>
2023-09-06 19:42:26 +0200

    message

--- printnbr.go
+++ printnbr.go
@@ -5,16 +5,11 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int) {
 	if n < 0 {
 		z01.PrintRune('-')
-	}
-	printDigits(n)
-}
-
-func printDigits(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-	}
-	if n/10 != 0 {
-		printDigits(n / 10)
+		PrintNbr(-n)
+	} else if n >= 10 {
+		PrintNbr(n / 10)
 		z01.PrintRune(rune(n%10 + '0'))
+	} else {
+		z01.PrintRune(rune(n + '0'))
 	}
 }
