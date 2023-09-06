4960e1105341c2dd9d9fa73a6da0644794d02028
srivier <soleane.rivier@ynov.com>
2023-09-06 20:56:08 +0200

    message

--- printcomb2.go
+++ printcomb2.go
@@ -6,7 +6,7 @@ func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				for l := '0'; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
 					if i <= k && j < l {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
