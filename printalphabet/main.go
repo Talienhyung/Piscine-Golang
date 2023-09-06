9f53e39be12ee1d14a01fb87f12d95a1f13b8dc0
srivier <soleane.rivier@ynov.com>
2023-09-06 16:30:30 +0200

    message

---
+++ printalphabet/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	var aRune string = "abcdefghijklmnopqrstuvwxyz"
+
+	for i := 0; i <= 25; i++ {
+		z01.PrintRune(rune(aRune[i]))
+	}
+	z01.PrintRune('\n')
+
+}
