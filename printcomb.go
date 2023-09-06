e6adcf214c8c4ae6ebeeb453629885233915b92c
srivier <soleane.rivier@ynov.com>
2023-09-06 18:41:21 +0200

    message

--- printcomb.go
+++ printcomb.go
@@ -3,13 +3,13 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := 0; i <= 10; i++ {
-		for j := 0; j <= 10; j++ {
-			for k := 0; k <= 10; k++ {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
 				if i < j && j < k {
-					z01.PrintRune(rune(i))
-					z01.PrintRune(rune(j))
-					z01.PrintRune(rune(k))
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
 					if i != 7 || j != 8 || k != 9 {
 						z01.PrintRune(rune(','))
 						z01.PrintRune(rune(' '))
