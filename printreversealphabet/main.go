10ceea26ab47fe1738471732f28fe650f6f30ae6
srivier <soleane.rivier@ynov.com>
2023-09-06 16:57:01 +0200

    message

---
+++ printreversealphabet/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	var aRune string = "abcdefghijklmnopqrstuvwxyz"
+	for i := 25; i >= 0; i-- {
+		z01.PrintRune(rune(aRune[i]))
+	}
+	z01.PrintRune('\n')
+}
