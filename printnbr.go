526df381536389f0757fea8426fc43a3bf2f0539
srivier <soleane.rivier@ynov.com>
2023-09-06 19:56:34 +0200

    message

--- printnbr.go
+++ printnbr.go
@@ -3,7 +3,11 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	if n < 0 {
+	if n == -2147483648 {
+		z01.PrintRune('-')
+		z01.PrintRune('2')
+		PrintNbr(147483648)
+	} else if n < 0 {
 		z01.PrintRune('-')
 		PrintNbr(-n)
 	} else if n >= 10 {
