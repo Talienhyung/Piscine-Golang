9fdb32b18942f93731699c0d9f02b6fdb3372a61
srivier <soleane.rivier@ynov.com>
2023-09-06 16:43:20 +0200

    message

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,10 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var aRune string = "abcdefghijklmnopqrstuvwxyz\n"
-
-	for i := 0; i <= 26; i++ {
+	var aRune string = "abcdefghijklmnopqrstuvwxyz"
+	for i := 0; i <= 25; i++ {
 		z01.PrintRune(rune(aRune[i]))
 	}
-
+	z01.PrintRune('\n')
 }
