1e1ac9075677e4562c50a286b90fafcb29cf831d
srivier <soleane.rivier@ynov.com>
2023-09-07 15:54:33 +0200

    non

--- alphacount.go
+++ alphacount.go
@@ -2,12 +2,17 @@ package piscine
 
 func AlphaCount(s string) int {
 	i := 0
+	e := 0
 	for range s {
 		if s[i] >= 'a' && s[i] <= 'z' {
 			i++
+			e++
 		} else if s[i] >= 'A' && s[i] <= 'Z' {
+			i++
+			e++
+		} else {
 			i++
 		}
 	}
-	return i
+	return e
 }
