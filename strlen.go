96b6db36d9031965305c2e1453dd3bf26124e82e
srivier <soleane.rivier@ynov.com>
2023-09-07 14:22:00 +0200

    non

--- strlen.go
+++ strlen.go
@@ -2,5 +2,14 @@ package piscine
 
 func StrLen(s string) int {
 	longueur := len(s)
-	return longueur
+	longueur2 := 0
+	table := "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn!"
+	for i := 0; i <= longueur; i++ {
+		for j := 0; j < 53; j++ {
+			if s[i] == table[j] {
+				longueur2++
+			}
+		}
+	}
+	return longueur2
 }
