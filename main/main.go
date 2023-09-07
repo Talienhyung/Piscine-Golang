1052cf99ae3f981b3bb0a08577c51be69c188964
srivier <soleane.rivier@ynov.com>
2023-09-07 12:53:29 +0200

    message

--- main/main.go
+++ main/main.go
@@ -1,14 +1,4 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-
-	"piscine"
-)
-
 func main() {
-	z01.PrintRune(piscine.LastRune("Hello!"))
-	z01.PrintRune(piscine.LastRune("Salut!"))
-	z01.PrintRune(piscine.LastRune("Ola!"))
-	z01.PrintRune('\n')
 }
