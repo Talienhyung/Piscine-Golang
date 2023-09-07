1802f91baf69b1c9f506b7fb5c9397036fd0607f
srivier <soleane.rivier@ynov.com>
2023-09-07 17:03:45 +0200

    non

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(s string) bool {
 	i := 0
 	for range s {
-		if s[i] == '\n' {
+		if s[i] == '\n' || s[i] == '\b' || s[i] == '\r' || s[i] == '\v' || s[i] == '\f' || s[i] == '\a' || s[i] == '\t' {
 			i++
 			return false
 		} else if s[i] >= 'A' && s[i] <= 'Z' {
