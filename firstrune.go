b208dba5245deedbac3c253df06be02d28dbd18a
srivier <soleane.rivier@ynov.com>
2023-09-07 12:44:09 +0200

    message

--- firstrune.go
+++ firstrune.go
@@ -2,5 +2,8 @@ package piscine
 
 func FirstRune(s string) rune {
 	lettre := s[0]
+	if rune(lettre) == 'â' {
+		return '♥'
+	}
 	return rune(lettre)
 }
--- main/main.go
+++ main/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"github.com/01-edu/z01"
+
+	"piscine"
+)
+
 func main() {
+	z01.PrintRune(piscine.FirstRune("Hello!"))
+	z01.PrintRune(piscine.FirstRune("Salut!"))
+	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune(piscine.FirstRune("♥01"))
+	z01.PrintRune('\n')
 }
