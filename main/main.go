d95c7969322d689fa07f6572e227770eb73c9b43
srivier <soleane.rivier@ynov.com>
2023-09-07 14:44:02 +0200

    non

--- main/main.go
+++ main/main.go
@@ -1,11 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"piscine"
-)
-
 func main() {
-	l := piscine.StrLen("Hello World!")
-	fmt.Println(l)
 }
--- strlen.go
+++ strlen.go
@@ -1,15 +1,9 @@
 package piscine
 
 func StrLen(s string) int {
-	longueur := len(s)
-	longueur2 := 0
-	table := "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn!"
-	for i := 0; i <= longueur; i++ {
-		for j := 0; j < 53; j++ {
-			if s[i] == table[j] {
-				longueur2++
-			}
-		}
+	len := 0
+	for range s {
+		len++
 	}
-	return longueur2
+	return len
 }
