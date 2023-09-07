3d462edd2be219b740d994d18b74f6da8d1bed34
srivier <soleane.rivier@ynov.com>
2023-09-07 15:16:54 +0200

    non

--- compare.go
+++ compare.go
@@ -1,28 +1,14 @@
 package piscine
 
 func Compare(a, b string) int {
-	lena := 0
-	for range a {
-		lena++
-	}
-	lenb := 0
-	for range b {
-		lenb++
-	}
-	lenea := lena - 1
-	leneb := lenb - 1
-	lettre := a[0]
-	lettre2 := b[0]
-	lettre3 := a[lenea]
-	lettre4 := b[leneb]
-	if lettre == lettre2 && lettre3 == lettre4 {
+	if a == b {
 		return 0
 	}
-	if lettre == lettre2 && lettre3 != lettre4 {
-		return 1
-	}
-	if lettre != lettre2 && lettre3 == lettre4 {
+	if a < b {
 		return -1
 	}
-	return 1
+	if a > b {
+		return 1
+	}
+	return 17
 }
