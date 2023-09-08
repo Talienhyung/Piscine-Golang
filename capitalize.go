0180a87966e9ee4c1462f853959f417bb4d1422d
srivier <soleane.rivier@ynov.com>
2023-09-08 15:10:53 +0200

    non

--- capitalize.go
+++ capitalize.go
@@ -22,7 +22,7 @@ func Capitalize(s string) string {
 		} else if s[i] >= 'A' && s[i] <= 'Z' {
 			if i > 0 {
 				if s[i-1] >= '0' && s[i-1] <= '9' {
-					car = append(car, string(s[i]))
+					car = append(car, string(s[i]+32))
 				} else if s[i-1] >= 'A' && s[i-1] <= 'Z' {
 					car = append(car, string(s[i]+32))
 				} else if s[i-1] >= 'a' && s[i-1] <= 'z' {
