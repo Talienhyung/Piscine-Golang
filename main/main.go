ade6da49e489418313b979fe34f38c3daf84594d
srivier <soleane.rivier@ynov.com>
2023-09-08 22:31:46 +0200

    comment jai reussi ??

--- main/main.go
+++ main/main.go
@@ -1,20 +1,4 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-
-	"piscine"
-)
-
 func main() {
-	piscine.PrintNbrBase(125, "0123456789")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(-125, "01")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(125, "0123456789ABCDEF")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(-125, "choumi")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(125, "aa")
-	z01.PrintRune('\n')
 }
