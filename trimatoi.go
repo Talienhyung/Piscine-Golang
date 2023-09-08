afe54244533a10a623ae10a686afa0c82bb7afaa
srivier <soleane.rivier@ynov.com>
2023-09-08 15:46:59 +0200

    roh

---
+++ trimatoi.go
@@ -0,0 +1,15 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	non := 0
+	nega := 1
+	for _, c := range s {
+		if c >= '0' && c <= '9' {
+			non = non*10 + int(c-'0')
+		}
+		if c == '-' && non == 0 {
+			nega = -1
+		}
+	}
+	return non * nega
+}
