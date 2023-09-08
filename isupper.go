5aca7996804e662996e5767fc744ca255aafcf61
srivier <soleane.rivier@ynov.com>
2023-09-08 11:38:33 +0200

    lol

--- isupper.go
+++ isupper.go
@@ -1,18 +1,15 @@
 package piscine
 
 func IsUpper(s string) bool {
-	len := 0
+	i := 0
+	e := 0
 	for range s {
-		len++
-	}
-	total := 0
-	car := "AZERTYUIOPQSDFGHJKLMWXCVBN"
-	for i := 0; i < len; i++ {
-		for j := 0; j < 26; j++ {
-			if s[i] == car[j] {
-				total++
-			}
+		if s[i] >= 'A' && s[i] <= 'Z' {
+			i++
+			e++
+		} else {
+			i++
 		}
 	}
-	return total == len
+	return e == i
 }
--- tolower.go
+++ tolower.go
@@ -1,5 +1,21 @@
 package piscine
 
 func ToLower(s string) string {
-	return s
+	i := 0
+	var non string
+	car := make([]string, 0)
+	for range s {
+		if s[i] >= 'A' && s[i] <= 'Z' {
+			car = append(car, string(s[i]+32))
+		} else {
+			car = append(car, string(s[i]))
+		}
+		i++
+	}
+	j := 0
+	for range car {
+		non = non + car[j]
+		j++
+	}
+	return non
 }
