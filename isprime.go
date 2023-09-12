45fdc7b3c6c9b2bed658cd63048fd2682bab1a23
srivier <soleane.rivier@ynov.com>
2023-09-12 12:07:54 +0200

    ur mom

--- isprime.go
+++ isprime.go
@@ -2,8 +2,8 @@ package piscine
 
 func IsPrime(nb int) bool {
 	if nb > 1 {
-		for i := 2; i < 100; i++ {
-			for j := 2; j < 100; j++ {
+		for i := 2; i < 10000; i++ {
+			for j := 2; j < 10000; j++ {
 				if i*j == nb {
 					return false
 				}
