fbf0a6d417e81acc9d511aa0e02c1699c6741b93
srivier <soleane.rivier@ynov.com>
2023-09-12 10:53:17 +0200

    ur mom

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,14 +2,17 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	r := 1
-	if nb < 0 {
-		return 0
-	} else if nb == 0 {
-		return 1
-	} else {
-		for i := 1; i <= nb; i++ {
-			r *= i
+	if nb <= 2147483647 {
+		if nb < 0 {
+			return 0
+		} else if nb == 0 {
+			return 1
+		} else {
+			for i := 1; i <= nb; i++ {
+				r *= i
+			}
+			return r
 		}
-		return r
 	}
+	return 0
 }
