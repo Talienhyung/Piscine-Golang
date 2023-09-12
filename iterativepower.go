27dc08bc3e13499532a1cddd677b9d6317bab9db
srivier <soleane.rivier@ynov.com>
2023-09-12 11:15:00 +0200

    ur mom

--- iterativepower.go
+++ iterativepower.go
@@ -2,7 +2,9 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	nnb := nb
-	if power >= 0 {
+	if nb == 0 {
+		return 1
+	} else if power >= 0 {
 		for i := 0; i < power-1; i++ {
 			nb *= nnb
 		}
