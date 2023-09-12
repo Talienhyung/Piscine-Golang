35df9bc86cc3a627f5e01b9bd8c29e35e62d1559
srivier <soleane.rivier@ynov.com>
2023-09-12 11:24:15 +0200

    ur mom

--- recursivepower.go
+++ recursivepower.go
@@ -1,12 +1,12 @@
 package piscine
 
-func RecPower(nb int, power int) int {
+func RecursivePower(nb int, power int) int {
 	if nb == 0 && power == 0 {
 		return 1
 	} else if power == 0 {
 		return 1
 	} else if power > 0 {
-		return nb * RecPower(nb, power-1)
+		return nb * RecursivePower(nb, power-1)
 	}
 	return 0
 }
