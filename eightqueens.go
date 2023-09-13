e691b37279eec6d766ce3172aa2db18ef5bb7b7f
srivier <soleane.rivier@ynov.com>
2023-09-13 09:18:08 +0200

    OMG

--- eightqueens.go
+++ eightqueens.go
@@ -3,219 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func EightQueens() {
-	// for i := 15863724; i < 87654321; i++ {
-	// 	tab := transformation(i)
-	// 	if valide(tab) && verification(tab) {
-	// 		printTableau(tab)
-	// 	}
-	// }
-	printTableau(15863724)
-	printTableau(16374852)
-	printTableau(16837425)
-	printTableau(17246835)
-	printTableau(17362584)
-	printTableau(17468253)
-	printTableau(17582463)
-	printTableau(18357246)
-	printTableau(18642753)
-	printTableau(23168574)
-	printTableau(23175864)
-	printTableau(23571468)
-	printTableau(23641857)
-	printTableau(23681475)
-	printTableau(23685147)
-	printTableau(23864175)
-	printTableau(24158637)
-	printTableau(24683175)
-	printTableau(24753168)
-	printTableau(24758613)
-	printTableau(25713864)
-	printTableau(25741863)
-	printTableau(26174835)
-	printTableau(26358147)
-	printTableau(26417538)
-	printTableau(26471358)
-	printTableau(26831475)
-	printTableau(27368514)
-	printTableau(27415863)
-	printTableau(27418536)
-	printTableau(27581463)
-	printTableau(28357146)
-	printTableau(28613574)
-	printTableau(31758246)
-	printTableau(32574186)
-	printTableau(32581746)
-	printTableau(32758146)
-	printTableau(34186275)
-	printTableau(34275186)
-	printTableau(34285716)
-	printTableau(34286175)
-	printTableau(34716258)
-	printTableau(34718526)
-	printTableau(34815726)
-	printTableau(34852617)
-	printTableau(35261748)
-	printTableau(35281746)
-	printTableau(35286471)
-	printTableau(35714286)
-	printTableau(35841726)
-	printTableau(36258174)
-	printTableau(36271485)
-	printTableau(36275184)
-	printTableau(36418572)
-	printTableau(36428571)
-	printTableau(36814752)
-	printTableau(36815724)
-	printTableau(36824175)
-	printTableau(37162584)
-	printTableau(37285146)
-	printTableau(37286415)
-	printTableau(38471625)
-	printTableau(38526174)
-	printTableau(41582736)
-	printTableau(41586372)
-	printTableau(42571386)
-	printTableau(42586137)
-	printTableau(42736815)
-	printTableau(42736851)
-	printTableau(42751863)
-	printTableau(42857136)
-	printTableau(42861357)
-	printTableau(43625817)
-	printTableau(43627518)
-	printTableau(43681572)
-	printTableau(45186372)
-	printTableau(45316827)
-	printTableau(45317286)
-	printTableau(45726318)
-	printTableau(45813627)
-	printTableau(46152837)
-	printTableau(46257138)
-	printTableau(46271358)
-	printTableau(46317582)
-	printTableau(46372851)
-	printTableau(46827135)
-	printTableau(46831752)
-	printTableau(47185263)
-	printTableau(47263185)
-	printTableau(47286135)
-	printTableau(47316852)
-	printTableau(47382516)
-	printTableau(47526138)
-	printTableau(47531682)
-	printTableau(48136275)
-	printTableau(48157263)
-	printTableau(48316257)
-	printTableau(48317526)
-	printTableau(48531726)
-	printTableau(51468273)
-	printTableau(51682473)
-	printTableau(51683742)
-	printTableau(51842736)
-	printTableau(51863724)
-	printTableau(52468317)
-	printTableau(52473861)
-	printTableau(52617483)
-	printTableau(52683147)
-	printTableau(52713864)
-	printTableau(52736814)
-	printTableau(52814736)
-	printTableau(53168247)
-	printTableau(53172864)
-	printTableau(53627148)
-	printTableau(53682417)
-	printTableau(53728641)
-	printTableau(53742861)
-	printTableau(53847162)
-	printTableau(54186372)
-	printTableau(54273681)
-	printTableau(54682713)
-	printTableau(54683172)
-	printTableau(54813627)
-	printTableau(56318427)
-	printTableau(56372481)
-	printTableau(56374182)
-	printTableau(57138642)
-	printTableau(57142863)
-	printTableau(57248136)
-	printTableau(57263148)
-	printTableau(57263184)
-	printTableau(57413862)
-	printTableau(57428613)
-	printTableau(58413627)
-	printTableau(58417263)
-	printTableau(61473825)
-	printTableau(61528374)
-	printTableau(62713584)
-	printTableau(62714853)
-	printTableau(62837415)
-	printTableau(63175824)
-	printTableau(63184275)
-	printTableau(63185247)
-	printTableau(63571428)
-	printTableau(63581427)
-	printTableau(63724815)
-	printTableau(63728514)
-	printTableau(63741825)
-	printTableau(64158273)
-	printTableau(64285713)
-	printTableau(64713528)
-	printTableau(64718253)
-	printTableau(64738251)
-	printTableau(65147382)
-	printTableau(65184273)
-	printTableau(65281473)
-	printTableau(65283741)
-	printTableau(65713824)
-	printTableau(65714283)
-	printTableau(65724813)
-	printTableau(65813724)
-	printTableau(67241853)
-	printTableau(67418253)
-	printTableau(67425813)
-	printTableau(68241753)
-	printTableau(71386425)
-	printTableau(71642853)
-	printTableau(72418536)
-	printTableau(72581463)
-	printTableau(72584136)
-	printTableau(72631485)
-	printTableau(73168524)
-	printTableau(73528641)
-	printTableau(73582461)
-	printTableau(73641852)
-	printTableau(73825164)
-	printTableau(74258136)
-	printTableau(74286135)
-	printTableau(75241386)
-	printTableau(75246831)
-	printTableau(75316824)
-	printTableau(75841362)
-	printTableau(76135824)
-	printTableau(76314852)
-	printTableau(76318524)
-	printTableau(76358142)
-	printTableau(76428531)
-	printTableau(76824135)
-	printTableau(76831425)
-	printTableau(81357246)
-	printTableau(81642753)
-	printTableau(82417536)
-	printTableau(82531746)
-	printTableau(82637415)
-	printTableau(82753164)
-	printTableau(83162574)
-	printTableau(83625147)
-	printTableau(84136275)
-	printTableau(84152637)
-	printTableau(84613572)
-	printTableau(84752613)
-	printTableau(86251473)
-	printTableau(86357142)
-	printTableau(87246135)
-	printTableau(87362514)
-	printTableau(87415263)
-	printTableau(87531642)
+	for i := 15863724; i < 87654321; i++ {
+		tab := transformation(i)
+		if valide(tab) && verification(tab) {
+			printTableau(tab)
+		}
+	}
 }
 
 func transformation(n int) []int {
@@ -232,48 +25,47 @@ func transformation(n int) []int {
 	return t2
 }
 
-// func valide(tab []int) bool {
-// 	for i := range tab {
-// 		if tab[i] == 0 || tab[i] == 9 {
-// 			return false
-// 		}
-// 	}
-// 	for i, c := range tab {
-// 		for j, d := range tab {
-// 			if i != j && c == d {
-// 				return false
-// 			}
-// 		}
-// 	}
-// 	return true
-// }
+func valide(tab []int) bool {
+	for i := range tab {
+		if tab[i] == 0 || tab[i] == 9 {
+			return false
+		}
+	}
+	for i, c := range tab {
+		for j, d := range tab {
+			if i != j && c == d {
+				return false
+			}
+		}
+	}
+	return true
+}
 
-// func validationManger(i int, j int, s []int) string {
-// 	if i+j < 8 {
-// 		if s[i+j] == s[i]+j || s[i+j] == s[i]-j {
-// 			return "false"
-// 		}
-// 	} else if i-j >= 0 {
-// 		if s[i-j] == s[i]+j || s[i-j] == s[i]-j {
-// 			return "false"
-// 		}
-// 	}
-// 	return "true"
-// }
+func validationManger(i int, j int, s []int) string {
+	if i+j < 8 {
+		if s[i+j] == s[i]+j || s[i+j] == s[i]-j {
+			return "false"
+		}
+	} else if i-j >= 0 {
+		if s[i-j] == s[i]+j || s[i-j] == s[i]-j {
+			return "false"
+		}
+	}
+	return "true"
+}
 
-// func verification(tab []int) bool {
-// 	for i := 1; i < 8; i++ {
-// 		for j := 1; j < 8; j++ {
-// 			if validationManger(i, j, tab) == "false" {
-// 				return false
-// 			}
-// 		}
-// 	}
-// 	return true
-// }
+func verification(tab []int) bool {
+	for i := 0; i < 8; i++ {
+		for j := 1; j < 8; j++ {
+			if validationManger(i, j, tab) == "false" {
+				return false
+			}
+		}
+	}
+	return true
+}
 
-func printTableau(nb int) {
-	tab := transformation(nb)
+func printTableau(tab []int) {
 	for i := 0; i < len(tab); i++ {
 		z01.PrintRune(rune(tab[i] + '0'))
 	}
