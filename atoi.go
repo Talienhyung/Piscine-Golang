8e79d6a3130da7cb53bfab4cd03ce4ceb63f15c6
srivier <soleane.rivier@ynov.com>
2023-09-13 14:59:57 +0200

    OMG

--- atoi.go
+++ atoi.go
@@ -2,14 +2,26 @@ package piscine
 
 func Atoi(s string) int {
 	non := 0
-	nega := 1
+	nega := 2
 	for _, c := range s {
+		if c != '-' && c != '+' {
+			if c < '0' || c > '9' {
+				return 0
+			}
+		}
 		if c >= '0' && c <= '9' {
 			non = non*10 + int(c-'0')
 		}
-		if c == '-' && non == 0 {
+		if c == '-' && nega == 2 {
 			nega = -1
+		} else if c == '-' || c == '+' && nega == -1 || nega == 1 {
+			return 0
+		} else if c == '+' && nega == 2 {
+			nega = 1
 		}
+	}
+	if nega == 2 {
+		nega = 1
 	}
 	return non * nega
 }
