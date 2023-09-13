6dfede9707a62ccc0d2878acd3072b4de0203934
srivier <soleane.rivier@ynov.com>
2023-09-13 15:23:08 +0200

    OMG

--- atoi.go
+++ atoi.go
@@ -3,21 +3,27 @@ package piscine
 func Atoi(s string) int {
 	non := 0
 	nega := 2
-	for _, c := range s {
-		if c != '-' && c != '+' {
-			if c < '0' || c > '9' {
+	for i := range s {
+		if s[i] != '-' && s[i] != '+' {
+			if s[i] < '0' || s[i] > '9' {
 				return 0
 			}
 		}
-		if c >= '0' && c <= '9' {
-			non = non*10 + int(c-'0')
+		if s[i] >= '0' && s[i] <= '9' {
+			non = non*10 + int(s[i]-'0')
 		}
-		if c == '-' && nega == 2 && c == rune(s[0]) {
-			nega = -1
-		} else if c == '+' && nega == 2 && c == rune(s[0]) {
-			nega = 1
-		} else if (c == '-' || c == '+') && (nega == -1 || nega == 1) {
-			return 0
+		if s[i] == '-' {
+			if i == 0 {
+				nega = -1
+			} else {
+				return 0
+			}
+		} else if s[i] == '+' {
+			if i == 0 {
+				nega = 1
+			} else {
+				return 0
+			}
 		}
 	}
 	if nega == 2 {
--- main/main.go
+++ main/main.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println(piscine.Atoi("0000000012345"))
 	fmt.Println(piscine.Atoi("012 345"))
 	fmt.Println(piscine.Atoi("Hello World!"))
-	fmt.Println(piscine.Atoi("+1234-"))
+	fmt.Println(piscine.Atoi("1234-"))
 	fmt.Println(piscine.Atoi("-1234"))
 	fmt.Println(piscine.Atoi("++1234"))
 	fmt.Println(piscine.Atoi("--1234"))
