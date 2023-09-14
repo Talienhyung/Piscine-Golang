afa59f1a64c2d8065117c9561327c7279bdb235f
srivier <soleane.rivier@ynov.com>
2023-09-14 14:09:09 +0200

    urmom

--- isprime.go
+++ isprime.go
@@ -14,3 +14,29 @@ func IsPrime(nb int) bool {
 	}
 	return false
 }
+
+// package piscine
+
+// func IsPrime(nb int) bool {
+// 	if nb < 0 {
+// 		return false
+// 	}
+
+// 	if nb <= 3 {
+// 		return nb > 1
+// 	}
+
+// 	if nb%2 == 0 || nb%3 == 0 {
+// 		return false
+// 	}
+
+// 	i := 5
+// 	for i*i <= nb {
+// 		if nb%i == 0 || nb%(i+2) == 0 {
+// 			return false
+// 		}
+// 		i = i + 6
+// 	}
+
+// 	return true
+// }
