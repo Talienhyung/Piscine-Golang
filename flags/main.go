ab2b84a82dc2a4587c1bc197b771ebc08ab9252c
srivier <soleane.rivier@ynov.com>
2023-09-14 20:35:51 +0200

    n

---
+++ flags/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	var list [2]string
+	args := os.Args[1:]
+	order := false
+	if len(args) > 0 && args[0] == "--insert" || args[0] == "-i" {
+		args = args[1:]
+		list[1] = args[0]
+	}
+	if len(args) > 0 && args[0] == "--order" || args[0] == "-o" {
+		args = args[1:]
+		order = true
+	}
+	list[0] = args[0]
+	if len(args) == 0 || args[0] == "--help" || args[0] == "-h" {
+		fmt.Println("--insert")
+		fmt.Println("  -i")
+		fmt.Println("	This flag inserts the string into the string passed as argument.")
+		fmt.Println("--order")
+		fmt.Println("  -o")
+		fmt.Println("	This flag will behave like a boolean, if it is called it will order the argument.")
+	}
+	l := list[0] + list[1]
+	newlist := []rune(l)
+	if order {
+		SortArgs(newlist)
+	}
+	for _, char := range newlist {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune('\n')
+}
+
+func SortArgs(args []rune) {
+	for i := 0; i < len(args); i++ {
+		for j := i + 1; j < len(args); j++ {
+			if args[i] < args[j] {
+				args[i], args[j] = args[j], args[i]
+			}
+		}
+	}
+}
