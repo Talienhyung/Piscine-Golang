81dd3e027b956831e532dc919ac01329cd4237fa
srivier <soleane.rivier@ynov.com>
2023-09-17 22:50:02 +0200

    n

--- issorted.go
+++ issorted.go
@@ -1,23 +1,19 @@
 package piscine
 
 func IsSorted(f func(int, int) int, arr []int) bool {
-
 	cesce := true
 	decresce := true
-
 	for i := 1; i < Lent4(arr); i++ {
 		if !(f(arr[i-1], arr[i]) >= 0) {
 			cesce = false
 		}
 	}
-
 	for i := 1; i < Lent4(arr); i++ {
 		if !(f(arr[i-1], arr[i]) <= 0) {
 			decresce = false
 		}
 	}
 	return cesce || decresce
-
 }
 func F(a, b int) int {
 	if a > b {
