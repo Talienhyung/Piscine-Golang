e7a7b0c3073b9236eb14154f73bbb96968660db0
srivier <soleane.rivier@ynov.com>
2023-09-17 21:44:03 +0200

    n

--- concatparams.go
+++ concatparams.go
@@ -3,7 +3,7 @@ package piscine
 func ConcatParams(args []string) string {
 	str := ""
 	for i := range args {
-		if len(str)-1 != i {
+		if len(args)-1 != i {
 			str += args[i] + "\n"
 		} else {
 			str += args[i]
