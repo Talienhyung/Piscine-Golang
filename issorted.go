dd8c16f368edb78a0e3566eee31562a530c7e38f
srivier <soleane.rivier@ynov.com>
2023-09-17 22:50:50 +0200

    n

--- issorted.go
+++ issorted.go
@@ -15,15 +15,7 @@ func IsSorted(f func(int, int) int, arr []int) bool {
 	}
 	return cesce || decresce
 }
-func F(a, b int) int {
-	if a > b {
-		return 1
-	} else if a == b {
-		return 0
-	} else {
-		return -1
-	}
-}
+
 func Lent4(d []int) int {
 	return len(d)
 }
