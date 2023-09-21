9c91afcb472ad9b1248f0ad05563c172befe2051
srivier <soleane.rivier@ynov.com>
2023-09-21 11:02:40 +0200

    Oh

--- displayfile/main.go
+++ displayfile/main.go
@@ -8,17 +8,17 @@ import (
 func main() {
 	arg := os.Args[1:]
 	if len(arg) > 1 {
-		fmt.Println("Too many arguments")
+		fmt.Print("Too many arguments")
 		return
 	} else if len(arg) == 0 {
-		fmt.Println("File name missing")
+		fmt.Print("File name missing")
 		return
 	}
 	fileName := arg[0]
 	data, err := os.ReadFile(fileName)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Print(err)
 		return
 	}
-	fmt.Println(string(data))
+	fmt.Print(string(data))
 }
