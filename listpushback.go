a0da6ae0a9ecafc6c0bd292b6dbce812cf2e0fba
srivier <soleane.rivier@ynov.com>
2023-09-24 19:55:45 +0200

    C'est si simple que j'arrive a y faire en meme temps que ma manicure

--- listpushback.go
+++ listpushback.go
@@ -3,9 +3,9 @@ package piscine
 func ListPushBack(l *List, data interface{}) {
 	element := &NodeL{Data: data}
 	if l.Head == nil {
-		l.Tail = element
+		l.Head = element
 	} else {
-		l.Head.Next = element
+		l.Tail.Next = element
 	}
-	l.Head = element
+	l.Tail = element
 }
