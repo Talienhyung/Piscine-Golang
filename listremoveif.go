51f49e695df6989eda7f47b7f4c99edb96d823b4
srivier <soleane.rivier@ynov.com>
2023-09-24 21:29:35 +0200

    C'est bien ce que je me disais

--- listremoveif.go
+++ listremoveif.go
@@ -11,11 +11,28 @@ package piscine
 // }
 
 func ListRemoveIf(l *List, data_ref interface{}) {
-	elem := l.Head
-	for elem != nil {
-		if elem.Data == data_ref {
-			elem.Data = nil
+	for l.Head != nil {
+		if l.Head.Data == data_ref {
+			l.Head = l.Head.Next
+		} else {
+			break
 		}
-		elem = elem.Next
+	}
+	element := l.Head
+	prev := element
+	for element != nil {
+		if element.Next != nil {
+			if element.Data != data_ref {
+				prev = element
+			} else {
+				prev.Next = element.Next
+			}
+			element = element.Next
+		} else {
+			if element.Data == data_ref {
+				prev.Next = element.Next
+			}
+			element = element.Next
+		}
 	}
 }
