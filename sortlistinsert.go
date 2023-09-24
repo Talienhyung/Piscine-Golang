c3918fe5e61721102c8c5958c04085fdb0124d5c
srivier <soleane.rivier@ynov.com>
2023-09-24 22:05:24 +0200

    Disons que je suis obstiné

--- sortlistinsert.go
+++ sortlistinsert.go
@@ -2,9 +2,24 @@ package piscine
 
 func SortListInsert(l *NodeI, data_ref int) *NodeI {
 	elem := l
-	for elem != nil {
-		elem = elem.Next
+	if elem.Data > data_ref {
+		nouveauTruc := &NodeI{elem.Data, elem.Next}
+		actuTruc := &NodeI{data_ref, nouveauTruc}
+		elem = actuTruc
+		return elem
 	}
-	elem.Data = data_ref
-	return ListSort(l)
+	ancienneL := l
+	actuelL := l.Next
+	for actuelL != nil {
+		if actuelL.Data > data_ref {
+			nouveauTruc := &NodeI{data_ref, actuelL}
+			ancienneL.Next = nouveauTruc
+			return l
+		}
+		actuelL = actuelL.Next
+		ancienneL = ancienneL.Next
+	}
+	nouveauTruc := &NodeI{data_ref, nil}
+	ancienneL.Next = nouveauTruc
+	return l
 }
