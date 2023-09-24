c64ccd636f2c9c971c20a2ca89cecafb8865a0f2
srivier <soleane.rivier@ynov.com>
2023-09-24 21:38:23 +0200

    effectivement

--- listmerge.go
+++ listmerge.go
@@ -1,14 +1,9 @@
 package piscine
 
 func ListMerge(l1 *List, l2 *List) {
-	elem := l1.Head
-	for elem != nil {
-		elem = elem.Next
-	}
 	elem2 := l2.Head
 	for elem2 != nil {
-		elem.Data = elem2.Data
-		elem = elem.Next
+		ListPushBack(l1, elem2.Data)
 		elem2 = elem2.Next
 	}
 }
