8d3acaf446f7c51063ca0d62c749659f0c2572ee
srivier <soleane.rivier@ynov.com>
2023-09-24 20:31:29 +0200

    Miam miam

---
+++ listreverse.go
@@ -0,0 +1,22 @@
+package piscine
+
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
+
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
+
+func ListReverse(l *List) {
+	ListeAlenvert := &List{}
+	element := l.Head
+	for l != nil {
+		ListPushFront(ListeAlenvert, element.Data)
+		element = element.Next
+	}
+	l.Head = ListeAlenvert.Head
+	l.Tail = ListeAlenvert.Tail
+}
