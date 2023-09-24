b52bd427cad6ca031630c3fa4342c2618bbdd4f3
srivier <soleane.rivier@ynov.com>
2023-09-24 20:11:26 +0200

    je crois que je commence a capter le bail

--- listlast.go
+++ listlast.go
@@ -1,5 +1,11 @@
 package piscine
 
 func ListLast(l *List) interface{} {
-	return l.Tail.Data
+	element := l.Tail
+	var answer interface{}
+	for element != nil {
+		answer = l.Tail.Data
+		element = element.Next
+	}
+	return answer
 }
