5a9d28469cc1b34b6442a1b4f84287caf9a450e6
srivier <soleane.rivier@ynov.com>
2023-09-25 09:50:29 +0200

    Vla chiant les listes chainées de ta mere

--- sortedlistmerge.go
+++ sortedlistmerge.go
@@ -2,9 +2,8 @@ package piscine
 
 func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
 	elem2 := n2
-	elem := n1
 	for elem2 != nil {
-		elem = SortListInsert(elem, elem2.Data)
+		n1 = SortListInsert(n1, elem2.Data)
 		elem2 = elem2.Next
 	}
 	return n1
