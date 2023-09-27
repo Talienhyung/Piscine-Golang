32e31b0f10736f8ad546076feb9a845fdc5e3cb7
srivier <soleane.rivier@ynov.com>
2023-09-27 10:48:40 +0200

    Heurseument y'a le parrain

---
+++ btreesearchitem.go
@@ -0,0 +1,13 @@
+package piscine
+
+func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root.Data == elem {
+		return root
+	}
+	if elem < root.Data {
+		return BTreeSearchItem(root.Left, elem)
+	} else if elem > root.Data {
+		return BTreeSearchItem(root.Right, elem)
+	}
+	return nil
+}
