a6051539c63f3a818ff51a0a5821976bb5d97536
srivier <soleane.rivier@ynov.com>
2023-09-27 12:28:19 +0200

    FIN FINAL DE LA MORT ABSOLUT PASSANT PAR LA HHHJHHHHHHGIFHJGFVIUGVUGVKB

--- main/main.go
+++ main/main.go
@@ -10,8 +10,10 @@ func main() {
 	piscine.BTreeInsertData(root, "1")
 	piscine.BTreeInsertData(root, "7")
 	piscine.BTreeInsertData(root, "5")
-	node := piscine.BTreeSearchItem(root, "1")
-	rplc := &piscine.TreeNode{Data: "3"}
-	root = piscine.BTreeTransplant(root, node, rplc)
+	node := piscine.BTreeSearchItem(root, "4")
+	fmt.Println("Before delete:")
+	piscine.BTreeApplyInorder(root, fmt.Println)
+	root = piscine.BTreeDeleteNode(root, node)
+	fmt.Println("After delete:")
 	piscine.BTreeApplyInorder(root, fmt.Println)
 }
