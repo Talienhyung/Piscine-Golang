73e272a5a2e21848ad7441d993604480b2084e00
srivier <soleane.rivier@ynov.com>
2023-09-27 10:54:38 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAh

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,10 +1,12 @@
 package piscine
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root == nil {
+		return nil
+	}
 	if root.Data == elem {
 		return root
-	}
-	if elem < root.Data {
+	} else if elem < root.Data {
 		return BTreeSearchItem(root.Left, elem)
 	} else if elem > root.Data {
 		return BTreeSearchItem(root.Right, elem)
